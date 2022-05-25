d9621a8a361d0b4fe73d9f4e1a13ba728bef025e
SamueleTrainotti <samuele.trainotti@marconirovereto.it>
2022-05-25 21:52:22 +0200

    pulizia codice

--- routes/calendarioM.js
+++ routes/calendarioM.js
@@ -3,19 +3,26 @@ const res = require('express/lib/response')
 const router = express.Router()
 const Prenotazione = require('../models/prenotazione')
 
+// adesso la ricerca per data funziona correttamente, bisogna pulire il codice
+
 // <input type="date" name="data" value="<%= prenotazione.data %>"></input> 
 // 
 // Pagina che permette di selezionare la data
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.data != null && req.query.data !== ''){
-        searchOptions.data = new RegExp(req.query.data)
+    if (req.query.data != null && req.query.data !== '') {
+        searchOptions.data = String(new RegExp(req.query.data))
+    } else {
+        searchOptions.data = String(new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate()) //
     }
-    /*if(req.query.utente != null && req.query.utente !== ''){
-        searchOptions.utente = new RegExp(req.query.utente, 'i')
-    }*/
-    try{
-        const prenotazioni = await Prenotazione.find(searchOptions) // filtrare sulla data
+    try {
+        if (searchOptions.data.endsWith('/'))
+            searchOptions.data = searchOptions.data.slice(0, -1)
+        if (searchOptions.data.startsWith('/'))
+            searchOptions.data = searchOptions.data.slice(1,)
+
+        const prenotazioni = await Prenotazione.find(searchOptions) // filtra sulla data
+
         res.render('calendarioM/index', { 
             prenotazione : prenotazioni, 
             searchOptions: req.query
@@ -25,15 +32,4 @@ router.get('/', async (req, res) => {
     }
 })
 
-
-/*
-router.get('/:dataPrenotazione', async (req, res) => {
-    try{
-        const prenotazioni = await Prenotazione.find({}) // filtrare sulla data
-        res.render('calendarioM/index', { prenotazione : prenotazioni })
-    } catch {
-        res.send('error ')
-    }
-})
-*/
 module.exports = router
