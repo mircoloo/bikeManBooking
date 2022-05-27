fbc94621fe7270bcb20fe537cdca9e1277ca7e98
unknown <michele.zucchelli-1@studenti.unitn.it>
2022-05-27 22:11:17 +0200

    aggiunta pagina vistacalendario utente, aggiunti collegamenti per tornare indietro

--- routes/calendarioU.js
+++ routes/calendarioU.js
@@ -3,15 +3,37 @@ const res = require('express/lib/response')
 const router = express.Router()
 const Prenotazione = require('../models/prenotazione')
 
-router.get('/', async (req, res) => {
+
+router.get('/', async(req, res) => {
+    let searchOptions = {}
+    if(req.query.data != null && req.query.data !== ''){
+        searchOptions.data = String(new RegExp(req.query.data))
+    } else {
+        searchOptions.data = String(new Date().getFullYear()+'-'+(new Date().getMonth()+1)+'-'+new Date().getDate()) //
+    }
     try{
-        res.render('calendarioU/index' , { prenotazione : new Prenotazione()})
+        searchOptions.data = searchOptions.data.endsWith('/') ? searchOptions.data.slice(0, -1) : searchOptions.data
+        searchOptions.data = searchOptions.data.startsWith('/') ? searchOptions.data.slice(1, ) : searchOptions.data
+        const prenotazioni = await Prenotazione.find(searchOptions) 
+        
+        res.render('calendarioU/index', { 
+            prenotazione : prenotazioni, 
+            searchOptions: req.query
+        })
+    } catch {
+        res.send('error ')
+    }
+})
+
+router.get('/prenota', async (req, res) => {
+    try{
+        res.render('calendarioU/prenota' , { prenotazione : new Prenotazione()})
     } catch {
         res.send('errore nel caricamento della pagina calendario')
     }
 })
 
-router.post('/', async (req, res) => {
+router.post('/prenota', async (req, res) => {
     const pren = new Prenotazione({
         data: req.body.data,
         utente: req.body.utente,
