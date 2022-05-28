3617177a8475c0f5e0392350ddd9064befc9ba7d
unknown <michele.zucchelli-1@studenti.unitn.it>
2022-05-28 22:42:23 +0200

    aggiunta funzionalita mostra calendario al caricamento pagina

--- routes/calendarioU.js
+++ routes/calendarioU.js
@@ -5,35 +5,37 @@ const Prenotazione = require('../models/prenotazione')
 
 
 router.get('/', async(req, res) => {
-    let searchOptions = {}
+    let so = {}
     if(req.query.data != null && req.query.data !== ''){
-        searchOptions.data = String(new RegExp(req.query.data))
+        so.data = String(new RegExp(req.query.data))
     } else {
-        searchOptions.data = String(new Date().getFullYear()+'-'+(new Date().getMonth()+1)+'-'+new Date().getDate()) //
+        let tmp = new Date()
+        if(tmp.getMonth() < 10){
+            so.data = String(new Date().getFullYear()+'-0'+(new Date().getMonth()+1)+'-'+new Date().getDate())
+            //console.log('minore di 10: ' + so.data)
+        } else {
+            so.data = String(new Date().getFullYear()+'-'+(new Date().getMonth()+1)+'-'+new Date().getDate())
+            //console.log('maggiore di 10: ' + so.data)
+        }
     }
     try{
-        searchOptions.data = searchOptions.data.endsWith('/') ? searchOptions.data.slice(0, -1) : searchOptions.data
-        searchOptions.data = searchOptions.data.startsWith('/') ? searchOptions.data.slice(1, ) : searchOptions.data
-        const prenotazioni = await Prenotazione.find(searchOptions) 
+        so.data = so.data.endsWith('/') ? so.data.slice(0, -1) : so.data
+        so.data = so.data.startsWith('/') ? so.data.slice(1, ) : so.data
+        const prenotazioni = await Prenotazione.find(so) 
+
+        //console.log(so.data)
         
         res.render('calendarioU/index', { 
             prenotazione : prenotazioni, 
-            searchOptions: req.query
+            searchOptions: so,
+            nuovaPren: new Prenotazione()
         })
     } catch {
         res.send('error ')
     }
 })
 
-router.get('/prenota', async (req, res) => {
-    try{
-        res.render('calendarioU/prenota' , { prenotazione : new Prenotazione()})
-    } catch {
-        res.send('errore nel caricamento della pagina calendario')
-    }
-})
-
-router.post('/prenota', async (req, res) => {
+router.post('/', async (req, res) => {
     const pren = new Prenotazione({
         data: req.body.data,
         utente: req.body.utente,
