af8e44346be330a81bd43c71ce16741d39f71d88
unknown <michele.zucchelli-1@studenti.unitn.it>
2022-06-01 21:56:12 +0200

    aggiunti 2 test

---
+++ routes/calendarioU.test.js
@@ -0,0 +1,16 @@
+const app = require('./calendarioU')
+const request = require('supertest')
+const mongoose = require('mongoose')
+
+test('GET /calendario/prenotazioni should return json', () => {
+    request(app).get('/calendario')
+        .expect('Content-Type', /json/)
+        .expect(200)
+})
+
+// per ora il test non è corretto, da aggiornare  
+test('GET /calendario/May 30 should return reservation table row stef@gang.it - tagliando - canyon ', () => {
+    request(app).get('/calendario?data=2022-05-30')
+        .expect('Content-Type', /json/)
+        .expect(200)
+})
